e1374b0f9845e8b8c7e69aafa991fd7b3fc14c84
Tong Li <litongjava001@gmail.com>
2025-07-08 16:58:55 -1000

    change to 25

--- src/main/java/com/tio/mail/wing/service/SmtpSendService.java
+++ src/main/java/com/tio/mail/wing/service/SmtpSendService.java
@@ -94,9 +94,7 @@ public class SmtpSendService {
         // 配置 JavaMail Session
         Properties props = new Properties();
         props.put("mail.smtp.host", mxHost);
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.port", "25");
         Session jmSession = Session.getInstance(props);
 
         // 用 raw DATA 构造 MimeMessage（需要客户端在 DATA 阶段提交完整的 headers + body）
